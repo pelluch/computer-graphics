477a4bb6610c93cb28619f9976efc067611f52b3
Pablo Lluch <pablo.lluch@gmail.com>
2013-04-11 12:16:29 -0400

    Added new files, separation for scene objects.

--- src/SceneLib/SceneUtils.cs
+++ src/SceneLib/SceneUtils.cs
@@ -139,24 +139,7 @@ namespace SceneLib
         
     }
 
-    public abstract class SceneObject
-    {
-        public string Name { get; set; }
-        public Vector Scale { get; set; }
-        public Vector Position { get; set; }
-        public Vector Rotation { get; set; }
-
-        public abstract Vector SurfaceNormal(Vector point, Vector cameraDirection);
-        public abstract bool IsHit(Ray ray, HitRecord record, float near, float far);
-     
-
-        public SceneObject()
-        {
-            Scale = new Vector(1, 1, 1);
-            Position = new Vector(0, 0, 0);
-            Rotation = new Vector(0, 0, 0);
-        }
-    }
+   
 
     
 
